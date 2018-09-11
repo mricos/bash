9758f65405bd9cfe437a6e173a98f23d2a748513
Mike Ricos via ux305 <mike.ricos@gmail.com>
2018-09-10 21:24:44 -0600

    Adds logtime-clear.

--- logtime/logtime.sh
+++ logtime/logtime.sh
@@ -16,26 +16,40 @@ date-ts2dt(){
 
 }
 
+logtime-clear(){
+  LT_START=""
+  LT_STOP=""
+  LT_STOP_MSG=""
+}
 logtime-start() {
-  TS_START=$(date-ts)
-  LT_START_MSG="$@"
-  echo "Start timer: $TS_START $@"
+  if [ ! -z "$LT_START" ]; then
+    echo "LT_START not empty. Use ltclear to clear."
+  else
+    LT_START=$(date-ts)
+    LT_TIMER=$LT_START
+    LT_DATA_ARRAY=(one two three)
+    LT_START_MSG="$@"
+    echo "Start timer: $LT_START $@"
+  fi
 }
-logtime-continue() {
-  echo "Todo: figure out pausing."
-#  TS_START=$(date-ts)
-#  TS_START=$(($TS_START - $SECONDS))
-#  SECONDS=$((TS_END - TS_START))
-#  echo "Start continuing: $SECONDS $@"
-}
+ 
+
 logtime-stop() {
-  TS_END=$(date-ts)
+  LT_STOP=$(date-ts)
   LT_STOP_MSG=$@
   SECONDS=$((TS_END - TS_START))
   hms=$(logtime-duration $SECONDS) 
   echo "Timer stopped with: $hms"
   echo "startmsg: $LT_START_MSG"
   echo "endmsg: $LT_STOP_MSG"
+}
+
+logtime-continue() {
+  echo "Todo: figure out pausing."
+#  LT_START=$(date-ts)
+#  LT_START=$(($LT_START - $SECONDS))
+#  SECONDS=$((LT_END - LT_START))
+#  echo "Start continuing: $SECONDS $@"
 }
 
 logtime-ts() {
@@ -65,6 +79,11 @@ logtime-ts-human() {
   echo "$TS $remain" >> $TIMELOG
 }
 
+logtime-logmsg(){
+  TS=$(date +%s)
+  echo "$TS $LT_PROJECT $@" >> $TIMELOG
+}
+
 logtime-ls(){
    cat $TIMELOG
 }
@@ -80,12 +99,41 @@ logtime-project(){
   LT_PROJECT=$1
 }
 
+logtime-status(){
+  TS=$(date +%s)
+
+  if [ -z "$LT_STOP" ]; then
+     LT_ELAPSED=$(($TS-$LT_START))
+  else
+     LT_ELAPSED=$(($LT_STOP - $LT_START))
+  fi
+ 
+  echo "TS: $TS"
+  echo "LT_START: $LT_START"
+  echo "LT_STOP: $LT_STOP"
+  echo "LT_PROJECT: $LT_PROJECT"
+  hms=$(logtime-duration $LT_ELAPSED)
+  echo "LT_ELAPSED: $LT_ELAPSED ($hms)"
+  echo "LT_START_MSG: $LT_START_MSG"
+  echo "LT_STOP_MSG: $LT_STOP_MSG"
+  echo "TIMELOG: $TIMELOG"
+  echo "LT_DATA_ARRAY: ${LT_DATA_ARRAY[@]}"
+}
+
+logtime-hms-to-seconds(){
+  seconds=$(echo $1 | awk -F'[hmd:]' \
+    '{ print ($1 * 3600) + ($2 * 60) + $3 }')
+ echo $seconds
+}
+
 alias lt="logtime-logmsg $@"
 alias ltstart="logtime-start $@"
 alias ltstop="logtime-stop $@"
+alias ltstatus="logtime-status"
 alias lth="logtime-hms $@"
 alias ltd="logtime-ts-human $@"
 alias ltp="logtime-project $@"
 alias ltcat="cat $TIMELOG"
 alias ltls="logtime-ls"
 alias ltlog="logtime-ls"
+alias ltclear="logtime-clear"
