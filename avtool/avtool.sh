ee10668d6ac5b7c769ad800e8e6cc16da925d7bb
Mike Ricos via ux305 <mike.ricos@gmail.com>
2018-12-16 02:24:54 -0700

    Adds recording on second monitor.

--- avtool/avtool.sh
+++ avtool/avtool.sh
@@ -1,4 +1,4 @@
-avtool-record-screen(){
+avtool-record-laptopscreen(){
   ffmpeg -s 1920x1080 \
          -framerate 25 \
          -f x11grab \
@@ -9,4 +9,23 @@ avtool-record-screen(){
          -acodec aac \
          -strict experimental \
          $1.flv
+}
+avtool-record-monitor(){
+  ffmpeg -s 1440x900 \
+         -framerate 25 \
+         -f x11grab \
+         -i :0.0+1920,0 \
+         -f alsa \
+         -ac 2 \
+         -i pulse \
+         -acodec aac \
+         -strict experimental \
+         $1.flv
+}
+
+avtool-help() {
+echo "
+Check out following to improve capture:
+https://unix.stackexchange.com/questions/73622/how-to-get-near-perfect-screen-recording-quality
+"
 }
