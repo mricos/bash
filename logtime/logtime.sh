73e20699d52b93300422a08b36506e33fa6d4c9e
Mike Ricos via ux305 <mike.ricos@gmail.com>
2018-12-29 21:17:53 -0700

    Cleaning up commit function.

--- logtime/logtime.sh
+++ logtime/logtime.sh
@@ -4,9 +4,9 @@ TIMELOG=~/time.txt
 # date --date=@$TS  <-- create datetime string from TS env var
 
 logtime-clear(){
-  LT_START=""
-  LT_STOP=""
-  LT_DURATION=""
+  LT_START=0
+  LT_STOP=0
+  LT_DURATION=0
   LT_START_MSG=""
   LT_STOP_MSG=""
   LT_MARK=0
@@ -16,8 +16,8 @@ logtime-clear(){
   LT_ARRAY=()
 }
 logtime-start() {
-  if [ ! -z "$LT_START" ]; then
-    echo "LT_START not empty. Use ltclear to clear."
+  if (( $LT_START != 0 )); then
+    echo "LT_START not empty. Use logtime-clear to clear."
   else
     LT_START=$(date +%s)
     LT_MARK=$LT_START
@@ -56,18 +56,16 @@ logtime-string() {
 }
 
 logtime-commit() {
-  if [ -z "$LT_STOP" ]; then
+  if (( $LT_STOP == 0 )); then
     echo "Not stopped before commit."
     return 1
   fi 
-  IFS=$'\n'  
-  local commitMsg=$(logtime-string $(date +%s) $@)
-  IFS=$IFS_ORIG
-
-  echo "start.$LT_START_MSG"
-  printf 'mark.%s\n' "${LT_ARRAY[@]}"
-  echo "stop.$LT_STOP.$LT_DURATION.\"$LT_STOP_MSG\""
-  echo "commit.$commitMsg"
+  
+  echo "$LT_START_MSG"
+  if [ -n "$LT_ARRAY" ]; then
+    printf "  %s\n" "${LT_ARRAY[@]}"
+  fi
+  echo "$LT_STOP.$LT_DURATION.\"$LT_STOP_MSG\""
 }
 
 
@@ -81,7 +79,7 @@ logtime-ts-human() {
 
 logtime-status(){
   TS=$(date +%s)
-
+  LT_DURATION=$((TS - LT_START))
   if [ -z "$LT_START" ]; then
     echo "
    No timer started. 
@@ -90,20 +88,13 @@ logtime-status(){
     return 1
   fi
  
-  if [ -z "$LT_STOP" ]; then
-     LT_ELAPSED=$(($TS-$LT_START)) # fails if LT_START not defined
-  else
-     LT_ELAPSED=$(($LT_STOP - $LT_START))
-  fi
- 
   echo "LT_START: $LT_START"
   echo "LT_STOP: $LT_STOP"
+  echo "LT_DURATION: $LT_DURATION ($hms)"
   echo "LT_MARK: $LT_MARK"
   echo "LT_LASTMARK: $LT_LASTMARK"
   echo "LT_MARK_DURATION: $LT_MARK_DURATION"
-  echo "LT_PROJECT: $LT_PROJECT"
   local hms=$(logtime-hms $LT_DURATION)
-  echo "LT_DURATION: $LT_DURATION ($hms)"
   echo "LT_START_MSG: $LT_START_MSG"
   echo "LT_STOP_MSG: $LT_STOP_MSG"
   echo "TIMELOG: $TIMELOG"
