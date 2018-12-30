149a75fcb322fb25caf329a7ba1b67355116efbe
Mike Ricos via ux305 <mike.ricos@gmail.com>
2018-12-30 01:09:02 -0700

    Placeholder for unit test

---
+++ logtime/test.sh
@@ -0,0 +1,9 @@
+source logtime.sh
+logtime-clear
+logtime-start Unit test 1.
+sleep 1
+logtime-mark Adding a time marker
+logtime-mark Adding a a second time marker
+logtime-status
+logtime-stop Ending unit test 1.
+logtime-commit Commiting unit test 1.
