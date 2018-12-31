2f2ade5c13e93b48821bef00e137e54e83202cbd
Mike Ricos via ux305 <mike.ricos@gmail.com>
2018-12-31 01:13:03 -0700

    Changes the way LT_START is checked.

--- logtime/logtime.sh
+++ logtime/logtime.sh
@@ -4,7 +4,7 @@ TIMELOG=~/time.txt
 # date --date=@$TS  <-- create datetime string from TS env var
 
 logtime-clear(){
-  LT_START=0
+  LT_START=""
   LT_STOP=0
   LT_DURATION=0
   LT_ELAPSED=0
@@ -17,7 +17,7 @@ logtime-clear(){
   LT_ARRAY=()
 }
 logtime-start() {
-  if (( $LT_START != 0 )); then
+  if [ ! -z $LT_START ]; then
     echo "LT_START not empty. Use logtime-clear to clear."
   else
     LT_START=$(date +%s)
@@ -44,14 +44,54 @@ logtime-stop() {
   LT_DURATION=$((LT_STOP - LT_START))
 }
 
-logtime-add(){
-  IFS=$'\n'  
-  LT_ARRAY+=($(logtime-string $@))
-  IFS=$IFS_ORIG
-}
-
 logtime-string() {
   echo "$1.\""${@:2}"\""
+}
+
+logtime-hms(){
+  local h=$(($1 / 3600));
+  local m=$((($1 % 3600) / 60));
+  local s=$(($1 % 60));
+  echo "${h}h${m}m${s}s";
+}
+
+logtime-hms-to-seconds(){
+  seconds=$(echo $1 | awk -F'[hmd:]' \
+    '{ print ($1 * 3600) + ($2 * 60) + $3 }')
+ echo $seconds
+}
+
+logtime-status(){
+
+  if [ -z "$LT_START" ]; then
+    echo "
+   No timer started. 
+   Use logtime-start <optional message of intention>
+"
+    return 1
+  fi
+
+  TS=$(date +%s)
+  LT_ELAPSED=$((TS - LT_START))
+  local elapsedHms=$(logtime-hms $LT_ELAPSED)
+  echo "LT_START: $LT_START"
+  echo "LT_STOP: $LT_STOP"
+  echo "LT_DURATION: $LT_DURATION ( $(logtime-hms $LT_DURATION) )"
+  echo "LT_ELAPSED: $LT_ELAPSED ( $elapsedHms )"
+  echo "LT_MARK: $LT_MARK"
+  echo "LT_LASTMARK: $LT_LASTMARK"
+  echo "LT_MARK_DURATION: $LT_MARK_DURATION"
+  echo "LT_START_MSG: $LT_START_MSG"
+  echo "LT_STOP_MSG: $LT_STOP_MSG"
+  echo "TIMELOG: $TIMELOG"
+  echo "LT_ARRAY:"
+  IFS_ORIG=$IFS
+  IFS="" 
+  for line in ${LT_ARRAY[@]}; do
+    printf "mark.%s\n" $line 
+  done; 
+  IFS=$IFS_ORIG
+  echo ""
 }
 
 logtime-commit() {
@@ -66,63 +106,6 @@ logtime-commit() {
     printf "mark.%s\n" "${LT_ARRAY[@]}"
   fi
   echo "stop.$LT_STOP.\"$LT_STOP_MSG\".$LT_DURATION"
-}
-
-
-logtime-ts-human() {
-  TS=$(date +%s --date="$1")
-  all=$@
-  first=$1
-  remain=${all#$first}
-  echo "$TS $remain" 
-}
-
-logtime-status(){
-  TS=$(date +%s)
-  LT_ELAPSED=$((TS - LT_START))
-  local elapsedHms=$(logtime-hms $LT_ELAPSED)
-  if [ -z "$LT_START" ]; then
-    echo "
-   No timer started. 
-   Use logtime-start <optional message of intention>
-"
-    return 1
-  fi
-
-   
-  echo "LT_START: $LT_START"
-  echo "LT_STOP: $LT_STOP"
-  echo "LT_DURATION: $LT_DURATION ( $(logtime-hms $LT_DURATION) )"
-  echo "LT_ELAPSED: $LT_ELAPSED ( $elapsedHms )"
-  echo "LT_MARK: $LT_MARK"
-  echo "LT_LASTMARK: $LT_LASTMARK"
-  echo "LT_MARK_DURATION: $LT_MARK_DURATION"
-  local hms=$(logtime-hms $LT_DURATION)
-  echo "LT_START_MSG: $LT_START_MSG"
-  echo "LT_STOP_MSG: $LT_STOP_MSG"
-  echo "TIMELOG: $TIMELOG"
-  echo "LT_ARRAY:"
-  IFS_ORIG=$IFS
-  IFS=$'\n' # remove spaces
-  IFS="" 
-  for line in ${LT_ARRAY[@]}; do
-    printf "mark.%s\n" $line 
-  done; 
-  IFS=$IFS_ORIG
-  echo ""
-}
-
-logtime-hms(){
-  local h=$(($1 / 3600));
-  local m=$((($1 % 3600) / 60));
-  local s=$(($1 % 60));
-  echo "${h}h${m}m${s}s";
-}
-
-logtime-hms-to-seconds(){
-  seconds=$(echo $1 | awk -F'[hmd:]' \
-    '{ print ($1 * 3600) + ($2 * 60) + $3 }')
- echo $seconds
 }
 
 # Porcelain
