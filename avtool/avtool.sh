f046b743605b2fcb82082ea2b3a045276294b04e
Mike Ricos <mike.ricos@gmail.com>
2022-02-14 15:28:32 -0800

    Changes default behavior.

--- avtool/avtool.sh
+++ avtool/avtool.sh
@@ -1,53 +1,109 @@
-avtool-record-laptopscreen(){
-  ffmpeg -s 1920x1080 \
-         -framerate 25 \
-         -f x11grab \
-         -i :0.0+0,0 \
-         -f alsa \
-         -ac 2 \
-         -i pulse \
-         -acodec aac \
-         -strict experimental \
-         $1.flv
+channels=${channels:-1}                # 1-mono, 2-stereo
+audio_input=${audio_input:-pulse}      # pulse or alsa
+samplerate=${samplerate:-48000}        # audio sample rate
+acodec=${acodec:-vorbis}               # vorbis, opus, libopus
+screen=eDP-1                           # avtool-list-screens
+
+avtool-help(){
+
+cat <<EOF
+
+Avtool:
+
+  - records screen and audio
+  - records uncompressed audio
+  - records opus audio 
+
+channels: $channels  {1,2}
+audio_input: $audio_input  {pulse, alsa}
+acodec: $acodec  {aac,vorbis,opus,s16le, s24e, f32le, pcm_s8}
+samplerate: $samplerate {8000, 16000, 48000}
+screen=$screen {avtool-list-screens}
+
+avtool-<tab><tab> to see a list possible commands.
+EOF
 }
-avtool-record-audio(){
+
+avtool-record-wav() {
+  ffmpeg \
+         -f alsa \
+         -ac $channels \
+         -ar $samplerate \
+         -i  $audio_input \
+         $1$(date +%s).wav 
+}
+
+avtool-record-ogg(){
+  timestamp=$(date +%s)
   ffmpeg -f alsa \
-         -ac 2 \
-         -i pulse \
-         -acodec aac \
+         -ac $channels  \
+         -i  $audio_input \
+         -ar $samplerate \
+         -acodec $acodec \
          -strict experimental \
-         $1.aac
+         $1$timestamp.ogg
 }
-avtool-record-monitor(){
-  ffmpeg -s 1440x900 \
+
+avtool-fifo(){
+  # atool-fifo fifo.{wav,ogg,mkv,avi}
+  ffmpeg \
+         -y \
+         -f alsa \
+         -ac $channels \
+         -ar $samplerate \
+         -i  $audio_input \
+         -acodec $acodec \
+         -strict experimental \
+         $1 
+}
+
+avtool-record-video(){
+  echo ffmpeg -s $screen_size \
          -framerate 25 \
          -f x11grab \
-         -i :0.0+1920,0 \
+         -i :$screen_coord \
          -f alsa \
-         -ac 2 \
+         -ac 1 \
          -i pulse \
-         -acodec aac \
+         -acodec $acodec  \
          -strict experimental \
-         $1.flv
+         $1$(date +%s).mkv
 }
 
-avtool-help() {
-echo "
-Check out following to improve capture:
-https://unix.stackexchange.com/questions/73622/how-to-get-near-perfect-screen-recording-quality
-"
+avtool-play(){
+  ffplay $1
 }
 
-avtool-list-inputs(){
+avtool-set-screen(){
+  [ -z $1 ] && screen=eDP-1 || screen=$1  
+
+  export screen_coord=$(avtool-list-screens | \
+                 grep connected      | \
+                 grep $screen        |  awk '{print $3'})
+
+  export screen_size=$(echo $screen_coord | awk -F+ '{print $1}')
+
+ echo Using screen: $screen
+}
+
+avtool-list-all(){
+  xrandr
   arecord -l
 }
-avtool-record-wav() {
-[ -z $1 ] && input=pulse || input=$1  
-[ -z $2 ] && output=$(date +%s) || output=$2  
-echo   ffmpeg \
-         -f alsa \
-         -ac 2 \
-         -i  $input \
-         -acodec wav \
-         $output.wav 
+
+avtool-list-screens(){
+  avtool-list-all | grep connected
+}
+
+avtool-notes() {
+cat <<EOF
+
+Uncompressed video:
+
+ffmpeg -f x11grab -s SZ -r 30 -i :0.0 -qscale 0 -vcodec huffyuv grab.avi
+
+via https://unix.stackexchange.com/questions/
+73622/how-to-get-near-perfect-screen-recording-quality
+
+EOF
 }
