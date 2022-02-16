2e2e6821108a903669c704a09bf360e4e84d6e90
Mike Ricos <mike.ricos@gmail.com>
2022-02-16 12:54:57 -0800

    Adds vcodec variable.

--- avtool/avtool.sh
+++ avtool/avtool.sh
@@ -3,15 +3,18 @@ audio_input=${audio_input:-pulse}      # pulse or alsa
 samplerate=${samplerate:-48000}        # audio sample rate
 acodec=${acodec:-vorbis}               # vorbis, opus, libopus
 fmt=${fmt:-mkv}                        #  flv, mkv
-framerate=24
+framerate=12
+#vcodec="libvpx-vp9 -crf 30 -b:v 300k" 
+vcodec="libx264 -crf 20 -preset medium -tune animation"
+params264="-preset default -tune animation"
 
-#screen=eDP-1                           # avtool-list-screens
+screen=eDP-1                           # avtool-list-screens
+screen_size=1920x1080
+#screen_coord="0,0"
+
+#screen=HDMI-1                           # avtool-list-screens
 #screen_size=3400x1440
-#screen_coord=":0.0"
-
-screen=HDMI-1                           # avtool-list-screens
-screen_size=3400x1440
-screen_coord=":0.0"
+#screen_coord="0,0"
 
 avtool-help(){
 
@@ -26,10 +29,13 @@ Avtool:
 channels: $channels  {1,2}
 audio_input: $audio_input  {pulse, alsa}
 acodec: $acodec  {aac,vorbis,opus,s16le, s24e, f32le, pcm_s8}
-samplerate: $samplerate {8000, 16000, 48000}
+vcodec: $vcodec  {https://trac.ffmpeg.org/wiki}
+samplerate: $samplerate {8000, 16000, 24000, 48000}
 framerate: $framerate {1 - 60}
-fmt=$fmt {flv, mkv}
-screen=$screen {avtool-list-screens}
+fmt: $fmt {flv, mkv}
+screen: $screen {avtool-list-screens}
+screen_size: $screen_size
+screen_coord: $screen_coord
 
 avtool-<tab><tab> to see a list possible commands.
 EOF
@@ -71,19 +77,21 @@ avtool-fifo(){
 # ffmpeg -framerate 12 -f x11grab -i :0 -f pulse -ac 2 -i default test.flv
 avtool-record-video(){
    timestamp=$(date +%s)
-   longname="${acodec}_${channels}_10s_${screen_size}_${framerate}"
+   longname="${acodec}_${channels}_${screen_size}_${framerate}"
    filename=${1:-$longname}_$timestamp.$fmt
-   ffmpeg -s $screen_size \
-         -framerate $framerate \
-         -f x11grab \
-         -i $screen_coord \
-         -f $audio_input \
-         -i default \
-         -ac $channels \
-         -ar $samplerate \
-         -acodec $acodec \
-         -strict experimental \
-         $filename >> avtool.log
+   ffmpeg \
+          -s $screen_size \
+          -framerate $framerate \
+          -f x11grab \
+          -i "$DISPLAY+$screen_coord" \
+          -f $audio_input \
+          -i default \
+          -ac $channels \
+          -ar $samplerate \
+          -acodec $acodec \
+          -strict experimental \
+          -vcodec $vcodec \
+          $filename 
 }
 
 avtool-play(){
@@ -102,9 +110,10 @@ avtool-set-screen(){
   read -r  screen_size x y   < <(IFS=+; echo $xrandr_coord)
   echo "avtool-set-screen: screen_size=$screen_size x=$x y=$y"
 
-  DISPLAY="${DISPLAY}"  # :0.0
-  screen_coord="${x},${y}"
+  #DISPLAY="${DISPLAY}"  # :0.0
+  screen_coord="${x},${y}" # full parameter for ffmpeg: :0.0+x,y
 
+ echo Using DISPLAY: $DISPLAY
  echo Using screen: $screen
  echo Using screen_size: $screen_size
  echo Using screen_coord: $screen_coord
