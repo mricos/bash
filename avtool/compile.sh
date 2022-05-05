26ea1fb5e22f54aa9b84da5242119ff103720c88
Mike <mike.ricos@gmail.com>
2022-05-05 01:51:40 -0700

    working with stk methods

--- avtool/compile.sh
+++ avtool/compile.sh
@@ -1,5 +1,6 @@
 STK_INCLUDE=./stk/include
 STK_SRC=./stk/src
+avtool-build(){
 g++ -std=c++11  -O3 -Wall  \
    -I$STK_INCLUDE \
    -DHAVE_GETTIMEOFDAY \
@@ -9,3 +10,12 @@ g++ -std=c++11  -O3 -Wall  \
    avtool.cpp \
    -L$STK_SRC \
    -lstk -lpthread -lasound -lm
+}
+
+avtool-compile-help(){
+cat <<EOF
+cut-n-paste to get the goods
+wget http://ccrma.stanford.edu/software/stk/release/stk-4.6.2.tar.gz
+tar xfvz stk-4.6.2.tar.gz
+EOF
+}
