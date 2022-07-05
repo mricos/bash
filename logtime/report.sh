5e621a72053d0eaaab0a230ba99d91ddfedee732
Mike Lenan <mike@lenan.net>
2022-07-04 23:14:59 -0700

    Adds report.sh

---
+++ logtime/report.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# set in logtime.sh
+#LT_DIR=~/.logtime
+#LT_STATES=$LT_DIR/states
+#LT_COMMITS=$LT_DIR/commits
+
+logtime-report-html-help(){
+   cat<<EOF
+logtime-report- are a set of Bash functions that summarizes 
+a list of time entries delinated by newline character (0x0A)
+
+480 took a short walk
+EOF
+}
