401c8f92d31361a0cf7ef0d8edfbb5da6ddb45a3
Mike Ricos <mike.ricos@gmail.com>
2024-01-05 22:26:37 -0800

    Fixes os date

--- logtime/date.sh
+++ logtime/date.sh
@@ -1,7 +1,2 @@
 # Create an alias for the 'date' command based on the operating system
-
-[ "$(uname)" = "Darwin" ] && \
-    alias date='gdate'  && echo "Darwin/Mac OS uses gdate" 2>&1 
-
-[ ! "$(uname)" = "Darwin" ] && \
-    unalias date && echo "Standard date" 2>&1
+[ "$(uname)" = "Darwin" ] && alias date='gdate'
