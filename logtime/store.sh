5a6c7716856ef44ebd6ff3cb9dd5dd4139277a91
Mike Ricos <mike.ricos@gmail.com>
2024-01-11 05:17:14 -0800

    Refactors logtime-store, adds multiline

--- logtime/store.sh
+++ logtime/store.sh
@@ -1,4 +1,31 @@
-logtime-store(){
+logtime-store() {
+    local timestamp=$(date +%s)
+    local file="$LT_DIR/store/$LT_START.store"
+    local input="$@"
+
+    # Single line input
+    echo "$timestamp $input" >> "$file"
+}
+
+logtime-store-multiline() {
+    local start_time=$(date +%s)
+    local file="$LT_DIR/store/$LT_START.store"
+
+    # Mark the start of a multiline input
+    echo "$start_time START_MULTILINE" >> "$file"
+
+    # Read and log each line
+    while IFS= read -r line; do
+        echo "$start_time $line" >> "$file"
+    done
+
+    # Mark the end of the multiline input
+    local end_time=$(date +%s)
+    echo "$end_time END_MULTILINE" >> "$file"
+}
+
+
+logtime-store-deprecated(){
   echo "$(date +%s) $@" >> $LT_DIR/store/$LT_START.store
 }
 
