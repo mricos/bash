7025476237097be56b0a99925a897b431eb2a1f4
Mike Ricos <mike@lenan.net>
2024-02-07 15:33:30 -0800

    Adds margin

--- qa/qa.sh
+++ qa/qa.sh
@@ -4,6 +4,7 @@
 QA_DIR="$HOME/.qa"
 alias q='qa_short'
 alias qq='qa_query'
+alias margin='qa_margin'
 alias db="ls $QA_DIR/db"
 
 # Default configurations overwriten by init()
@@ -226,6 +227,38 @@ qa_log_show(){
    cat $_QA_LOG
 }
 
+qa_margin() {
+  top=$1
+  right=$2
+  bottom=$3
+  left=$4
+ 
+  # Calculate the number of blank lines based on the top argument
+  for (( i=0; i<top; i++ )); do
+    echo
+  done
+
+  # Calculate the desired width for text formatting
+  # This calculation considers both left and right margins
+  let text_width=$COLUMNS-$left-$right
+
+  # Check if calculated text width is positive
+  if [ $text_width -le 0 ]; then
+    echo "Error: Left and right margins exceed the available column width."
+    return 1
+  fi
+
+  # Use sed to add left margin by padding spaces to the beginning of each line
+  # Then use fmt to wrap text according to the calculated width
+  sed "s/^/$(printf '%*s' $left)/" | fmt -w $text_width
+
+  # Calculate the number of blank lines based on the bottom argument
+  for (( j=0; j<bottom; j++ )); do
+    echo
+  done
+}
+
+
 a_old ()
 {
     cat "$QA_DIR/last_answer"
@@ -270,6 +303,7 @@ aq ()
     #cat "${files[$index]}" | jq -r '.messages[0].content'
     cat "${files[$index]}" 
 }
+
 
 _qa_sanitize_index ()
 {
